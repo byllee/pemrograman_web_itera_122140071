af04a3afafa11f4999f0550fc19b74cab719755f
byllee <febrianisianipar524@gmail.com>
2025-04-27 21:12:51 +0700

    Pindahkan file ke folder baru febrianinws_122140071_pertemuan4

---
+++ bmi.py
@@ -0,0 +1,33 @@
+class BMI:
+    def __init__(self, berat, tinggi):
+        self.berat = berat
+        self.tinggi = tinggi
+
+    def hitung_bmi(self):
+        return self.berat / (self.tinggi ** 2)
+
+    def kategori_bmi(self, bmi):
+        if bmi < 18.5:
+            return "Berat badan kurang"
+        elif 18.5 <= bmi < 25:
+            return "Berat badan normal"
+        elif 25 <= bmi < 30:
+            return "Berat badan berlebih"
+        else:
+            return "Obesitas"
+
+    def tampilkan_hasil(self):
+        bmi = self.hitung_bmi()
+        kategori = self.kategori_bmi(bmi)
+        print(f"\nBMI Anda adalah: {bmi:.2f}")
+        print(f"Kategori BMI: {kategori}")
+
+
+# Input dari pengguna
+berat = float(input("Masukkan berat badan (kg): "))
+tinggi = float(input("Masukkan tinggi badan (m): "))
+
+bmi_calculator = BMI(berat, tinggi)
+
+# Menampilkan hasil perhitungan BMI
+bmi_calculator.tampilkan_hasil()
